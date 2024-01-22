d14a89ea9c0b594c95e983bb6c4049f58b739e3f
noah evans <noahjevans@gmail.com>
2024-01-22 00:58:40 -0800

    Refactor Web component to HomePage with updated UI and functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-export default function Web() {
-  return (
-    <>
-      
-    </>
-  )
+import React from 'react';
+
+export default function HomePage() {
+   return (
+
+    <div className="bg-orange-500 h-screen flex flex-col items-center justify-center text-white">
+      <h1>Hello World</h1>
+      <button
+        className="mt-4 py-2 px-4 border border-black rounded"
+        onClick={() => window.location.href = 'https://www.google.com'}
+      >
+        Press me
+      </button>
+    </div>
+   )
 }
