e37fc01bf0919d533c97c5199549266569a920ee
noah evans <noahjevans@gmail.com>
2024-01-22 01:05:05 -0800

    first pass

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function HomePage() {
-  const router = useRouter();
    return (
-
     <div className="bg-orange-500 h-screen flex flex-col items-center justify-center text-white">
-      <h1>Hello World</h1>
+      <h1>Do you ha(l)ve Bitcoin?</h1>
       <button
         className="mt-4 py-2 px-4 border border-black rounded"
-        onClick={() => window.location.href = 'https://www.google.com'}
       >
-        Press me
+        We Should Party
       </button>
     </div>
    )
