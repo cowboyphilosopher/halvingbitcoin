9066c80059837b4d4612f210fa446398d695464c
noah evans <noahjevans@gmail.com>
2024-01-22 01:11:18 -0800

    just this for now

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,7 @@ import { useRouter } from 'next/navigation';
 export default function HomePage() {
    return (
     <div className="bg-orange-500 h-screen flex flex-col items-center justify-center text-white">
-      <h1>Do you ha(l)ve Bitcoin?</h1>
-      <button
-        className="mt-4 py-2 px-4 border border-black rounded"
-      >
-        We Should Party
-      </button>
+      <h1>Do you ha(l)ve Bitcoin? We should party.</h1>
     </div>
    )
 }
