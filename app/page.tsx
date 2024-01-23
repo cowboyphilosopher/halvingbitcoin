749bbf27f9bf1e03fbf3e781886d7d714f0cd024
noah evans <noahjevans@gmail.com>
2024-01-22 17:23:16 -0800

    Refactor button to use Button component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
 /* use client */
 import React from 'react';
-import { useRouter } from 'next/navigation'
+import { Button } from "components/Button/Button"
  
 export default function HomePage() {
-  const router = useRouter()
  
   return (
     <div className="bg-orange-500 h-screen flex flex-col items-center justify-center text-white">
       <h1>Do you ha(l)ve Bitcoin? We should party.</h1>
-      <button
-        type="button"
-        className="mt-4 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-black bg-white hover:bg-gray-100"
-        onClick={() => router.push('/dashboard')}
-      >
-        Dashboard
-      </button>
+      <Button href="https://github.com/Blazity/next-enterprise" className="mr-3">
+              Get started
+            </Button>
     </div>
   )
 }
