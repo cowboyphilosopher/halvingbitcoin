a5d3498a19d39015c0563720e8d9c4f66407ff3e
noah evans <noahjevans@gmail.com>
2024-01-22 17:19:02 -0800

    Refactor page.tsx to use next/navigation instead of next/router.

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import { useRouter } from 'next/router';
-
+import { useRouter } from 'next/navigation'
+ 
 export default function HomePage() {
-  const router = useRouter();
-
+  const router = useRouter()
+ 
   return (
-
     <div className="bg-orange-500 h-screen flex flex-col items-center justify-center text-white">
       <h1>Do you ha(l)ve Bitcoin? We should party.</h1>
-      <button
-        className="mt-4 py-2 px-4 border border-black rounded"
-        onClick={() => router.push('https://www.google.com')}
-      >
-        Press me
-      </button>
     </div>
-   )
-}
+    <button type="button" onClick={() => router.push('/dashboard')}>
+      Dashboard
+    </button>
+  )
 }
