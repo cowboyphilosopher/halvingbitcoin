95983091a09f40cda9d4a72bc3f41dcbbb294af4
noah evans <noahjevans@gmail.com>
2024-01-23 00:31:47 -0800

    Refactor Countdown component styles and text.

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -11,18 +11,19 @@ const Countdown = () => {
 
   useEffect(() => {
     // You can add any additional effects if needed when blockHeight changes
+    
   }, [blockHeight]);
 
   return (
     <div className="flex flex-col h-full">
-      <div className="flex justify-center items-center bg-gray-200 text-xl font-semibold h-1/6 rounded-t-lg">
-        Countdown
+      <div className="flex justify-center items-center bg-orange-500 text-xl text-white font-bold h-1/5 rounded-t-lg">
+        Blocks Remaining
       </div>
-      <div className="flex flex-1 justify-center items-center bg-gray-300 rounded-b-lg">
-        <span className={`text-green-500 bg-black px-2 py-1 ${error ? 'text-red-500' : ''}`}>
+      <div className="flex flex-1 justify-center items-center bg-orange-500 rounded-b-lg">
+        <span className={`text-white bg-alpha px-2 py-1 ${error ? 'text-red-500' : ''}`}>
           {error ? 'Error fetching data' : blockHeight}
         </span>
-        <span className="text-gray-700 ml-2">Current Block Height</span>
+        <span className="text-white ml-2"> Blocks until the 1st event.</span>
       </div>
     </div>
   );
